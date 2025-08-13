67d56db36fe560da07b339a13358cac911f50cdd
Eric Mara <ericmaraoliveira@gmail.com>
2025-08-12 21:57:39 -0300

    procurar usuarior por id

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express from 'express'
+import express, { json } from 'express'
 import { prismaClient } from '../prisma/prisma.js';
 
 
@@ -7,10 +7,28 @@ const app = express();
 app.use(express.json());
 
 app.get('/usuarios', async (req, res) => {
-    try{
-        const [usuarios] = await prismaClient.usuario.findMany();
+    try {
+        const usuarios = await prismaClient.usuario.findMany();
         return res.json(usuarios)
-    }catch (e){
+    } catch (e) {
+        console.log(e)
+    }
+})
+
+app.get("/usuarios/:id", async (req, res) => {
+    try {
+        const usuario = await prismaClient.usuario.findUnique({
+            where: {
+                id: Number(req.params.id)
+            }
+        });
+
+        if(usuario == null){
+            return res.json("Usuario não encontrado")
+        }
+
+        return res.json(usuario)
+    } catch (e){
         console.log(e)
     }
 })
