d31cd851afe95d681010ef1aa0c065d9e30e7862
Eric Mara <ericmaraoliveira@gmail.com>
2025-08-19 22:02:35 -0300

    função de envia e atualizar usuario

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-import express, { json } from 'express'
+import express from 'express';
 import { prismaClient } from '../prisma/prisma.js';
 
 
@@ -9,11 +9,11 @@ app.use(express.json());
 app.get('/usuarios', async (req, res) => {
     try {
         const usuarios = await prismaClient.usuario.findMany();
-        return res.json(usuarios)
+        return res.json(usuarios);
     } catch (e) {
-        console.log(e)
+        console.log(e);
     }
-})
+});
 
 app.get("/usuarios/:id", async (req, res) => {
     try {
@@ -23,13 +23,59 @@ app.get("/usuarios/:id", async (req, res) => {
             }
         });
 
-        if(usuario == null){
-            return res.json("Usuario não encontrado")
-        }
+        if (!usuario) return res.status(404).send("Usuario não encontrado");
+        return res.json(usuario);
+    } catch (e) {
+        console.log(e);
+    }
+});
 
-        return res.json(usuario)
-    } catch (e){
-        console.log(e)
+app.post("/usuarios", async (req, res) => {
+    try {
+        const { body } = req;
+        const usuario = await prismaClient.usuario.create({
+            data: {
+                nome: body.nome,
+                cargo: body.cargo,
+                email: body.email,
+                senha: body.senha
+            },
+        })
+        return res.status(201).json(usuario)
+    } catch (error) {
+        console.log(error);
+        if (error.code === "P2002") return res.status(404).send("Falha ao cadastrar usuário: Email já cadastrado!");
+
+    }
+});
+
+app.put("/usuarios/:id", async (req, res) => {
+    try {
+        const { body, params } = req;
+
+        await prismaClient.usuario.update({
+            where: { id: Number(params.id) },
+            data: {
+                ...body
+            },
+        })
+
+        const usuarioAtualizado = await prismaClient.usuario.findUnique({
+            where: {
+                id: Number(params.id)
+            }
+        })
+
+        res.status(201).json({
+            message: "Usuário atualizado!",
+            data: usuarioAtualizado
+        })
+        
+    } catch (error) {
+        console.log(error)
+        if (error.code === "P2025") return res.status(404).send("Usuario não encontrado");
+
+        if (error.code === "P2002") return res.status(404).send("Falha ao atualizar usuário: Email já cadastrado!");
     }
 })
 
