0cfb817c0b42caf84f5e9fbe00b6c158de7bbabe
eric <ericmaraoliveira@gmail.com>
2025-11-04 21:14:07 -0300

    aplicação swagger

--- src/app.js
+++ src/app.js
@@ -1,37 +1,29 @@
-import express from 'express';
+// Path: src/app.js
+import express from "express";
 import cors from "cors";
-import { consultaRouter } from './routes/consulta.js';
-import { exameRouter } from './routes/exames.js';
-import { pacienteRouter } from './routes/paciente.js';
-import { prontuarioRouter } from './routes/prontuario.js';
-import { usuarioRouter } from './routes/usuario.js';
-import { authRouter } from './routes/authRoutes.js';
-import { auth } from './middleware/auth.js';
-
+import { usuarioRouter } from "./routes/usuario.js";
+import { pacienteRouter } from "./routes/paciente.js";
+import { consultaRouter } from "./routes/consulta.js";
+import { prontuarioRouter } from "./routes/prontuario.js";
+import { exameRouter } from "./routes/exames.js";
+import authRouter from "./routes/authRoutes.js";
+import { auth } from "./middleware/auth.js";
+import { swaggerDocs } from "./config/swaggerConfig.js";
 export const app = express();
-
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
-
-//Router usuarios
-app.use('/auth', authRouter);
-app.use(auth)
-
-//Router cinsultas
-app.use(consultaRouter);
-
-//Router exames
-app.use(exameRouter);
-
-//Router pacientes
-app.use(pacienteRouter);
-
-//Router prontuario
-app.use(prontuarioRouter);
-
-//Router usuarios
+app.get("/ping", (req, res) => {
+ res.send("pong");
+});
+// Swagger UI
+swaggerDocs(app);
+// Rotas públicas
+app.use("/auth", authRouter);
+// Middleware global de autenticação
+app.use(auth);
+// Rotas privadas
 app.use(usuarioRouter);
-
-
-
-// app.listen(5000, () => console.log('Servidor rodando na porta 5000'))
+app.use(exameRouter);
+app.use(pacienteRouter);
+app.use(prontuarioRouter);
+app.use(consultaRouter);
